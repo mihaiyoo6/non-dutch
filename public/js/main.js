1bead06918b0bb8541f0af33c7d5612a8e88ef9a
Mihai Mijea <mmihai@suitsupply.com>
2018-02-21 06:58:20 +0100

    add babel and nunjucks

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 'use strict';
 import '../scss/style.scss';
+import helloTpl from '../templates/test.njk';
 
+const html = helloTpl.render({ message: 'world'});
 // eslint-disable-next-line no-console
-console.log('hello world!');
+console.log('hello world!', html);
+document.querySelector('#app').insertAdjacentHTML('afterbegin', html);
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/public/index.html',
-  filename: 'index.html',
-  inject: 'body'
+	template: __dirname + '/public/index.html',
+	favicon: __dirname + '/public/favicon.ico',
+	filename: 'index.html',
+	inject: 'body'
 });
 
 const ExtractTextPluginConfig = new ExtractTextPlugin('style.css');
 
 module.exports = {
-  entry: __dirname + '/public/js/main.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.scss$/,
-        include: __dirname + '/public',
-        loader: ExtractTextPlugin.extract('css-loader!sass-loader')
-      }
-    ]
-  },
-  plugins: [HtmlWebpackPluginConfig, ExtractTextPluginConfig]
+	entry: __dirname + '/public/js/main.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['env']
+				}
+			},
+			{
+				test: /\.scss$/,
+				include: __dirname + '/public',
+				loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+			},
+			{
+				test: /\.(njk|nunjucks)$/,
+				loader: 'nunjucks-loader'
+			}
+		]
+	},
+	plugins: [HtmlWebpackPluginConfig, ExtractTextPluginConfig]
 }
